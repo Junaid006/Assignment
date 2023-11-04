53ccbfc497b1e400aed43943e083126ddaa6959c
Junaid006 <89835497+Junaid006@users.noreply.github.com>
2023-11-04 12:06:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function navAlert(pageName) {
+    alert("Welcome to my " + pageName + "!");
+}
+
+function highlight(section) {
+    section.style.backgroundColor = "#f2f2f2";
+    section.style.color = "#333";
+}
+
+function unhighlight(section) {
+    section.style.backgroundColor = "";
+    section.style.color = "";
+}
